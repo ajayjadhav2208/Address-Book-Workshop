5691abed504caeb10faa9d07913e41c67c8a4ca4
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-10-27 08:24:44 +0530

    Welcome msg

---
+++ src/com/bridgelabz/AddressBook_Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class AddressBook_Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address book workshop");
+    }
+}
